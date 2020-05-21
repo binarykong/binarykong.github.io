15c4a66c4bf674b040191fc4d4de67df37dc28af
Chang Liu <chang.liu@moblab.com>
2020-05-20 17:51:49 -0700

    v0.0.1

---
+++ precache-manifest.3f22346a45ae84d698710ca1d8f56f1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41c69501866c5e59ff2555fa7ae8986f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10d6c93aab5223de84f2",
+    "url": "/static/css/main.382c5286.chunk.css"
+  },
+  {
+    "revision": "86c13c2ae2e2b095a314",
+    "url": "/static/js/2.444ea0d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.444ea0d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10d6c93aab5223de84f2",
+    "url": "/static/js/main.b3348cc5.chunk.js"
+  },
+  {
+    "revision": "d6bd80d3f6a1af9d7ae4",
+    "url": "/static/js/runtime-main.20a5da30.js"
+  }
+]);
